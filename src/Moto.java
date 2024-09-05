166c1c43c222114c32da141b70fead44841088f8
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-09-05 12:23:01 +0100

    ajout des conditions dans la methode calculerPrixLocation

--- src/Moto.java
+++ src/Moto.java
@@ -4,8 +4,8 @@ public class Moto extends Vehicule {
 
     private boolean toutTerrain;
 
-    public Moto(int id, String marque, String modele, Data annee, boolean toutTerrain) {
-        super(id, marque, modele, annee);
+    public Moto(int id, String marque, String modele, Data annee, float prixDeBase, boolean toutTerrain) {
+        super(id, marque, modele, annee, prixDeBase);
         this.toutTerrain = toutTerrain;
     }
 
@@ -18,7 +18,12 @@ public class Moto extends Vehicule {
     }
 
     public Float calculerPrixLocation(int jours){
-        return getPrixDeBase() * jours;
+
+        float prixLocation =  prixDeBase * jours;
+        if(toutTerrain){
+            prixLocation *= 0.2;
+        }
+        return prixLocation;
     }
 
     public void afficherType(){
@@ -27,9 +32,10 @@ public class Moto extends Vehicule {
 
     public void obtenirDescription(){
         System.out.println("Description de la Moto");
-        System.out.println("Marque : " + getMarque());
-        System.out.println("Modele : " + getModele());
-        System.out.println("Annee : " + getAnnee());
+        System.out.println("Marque : " + marque);
+        System.out.println("Modele : " + modele);
+        System.out.println("Annee : " + annee);
+        System.out.println("Prix deBase : " + prixDeBase);
         System.out.println("ToutTerrain : " + isToutTerrain());
     }
 
