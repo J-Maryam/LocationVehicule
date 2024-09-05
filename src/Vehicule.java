2d990f0de7da57192a2acc9f8bbd965a0b819278
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-09-05 12:11:54 +0100

    uodate vehicule class

--- src/Vehicule.java
+++ src/Vehicule.java
@@ -2,57 +2,17 @@ import javax.xml.crypto.Data;
 
 abstract public class Vehicule {
 
-    private int id;
-    private String marque;
-    private String modele;
-    private Data annee;
-    private Float prixDeBase;
+    protected int id;
+    protected String marque;
+    protected String modele;
+    protected Data annee;
+    protected Float prixDeBase;
 
-    public Vehicule(int id, String marque, String modele, Data annee) {
+    public Vehicule(int id, String marque, String modele, Data annee, float prixDeBase) {
         this.id = id;
         this.marque = marque;
         this.modele = modele;
         this.annee = annee;
-        this.prixDeBase = prixDeBase;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMarque() {
-        return marque;
-    }
-
-    public void setMarque(String marque) {
-        this.marque = marque;
-    }
-
-    public String getModele() {
-        return modele;
-    }
-
-    public void setModele(String modele) {
-        this.modele = modele;
-    }
-
-    public Data getAnnee() {
-        return annee;
-    }
-
-    public void setAnnee(Data annee) {
-        this.annee = annee;
-    }
-
-    public Float getPrixDeBase() {
-        return prixDeBase;
-    }
-
-    public void setPrixDeBase(Float prixDeBase) {
         this.prixDeBase = prixDeBase;
     }
 
